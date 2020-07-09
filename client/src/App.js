fadd4d79d580ff47726f7dfc3907574025b5cf9b
Romo <60673021+Z-Camp@users.noreply.github.com>
2020-07-09 19:25:56 -0400

    icons, some routing, some renaming

--- client/src/App.js
+++ client/src/App.js
@@ -11,48 +11,50 @@ import AuthService from './utils/auth';
 import UserInfoContext from './utils/UserInfoContext';
 
 function App() {
-  // set data to be used for UserInfoContext and make it available to all other components
-  const [userInfo, setUserInfo] = useState({
-    savedBooks: [],
-    username: '',
-    email: '',
-    bookCount: 0,
-    // method to get user data after logging in
-    getUserData: () => {
-      // if user's logged in get the token or return null
-      const token = AuthService.loggedIn() ? AuthService.getToken() : null;
+	// set data to be used for UserInfoContext and make it available to all other components
+	const [userInfo, setUserInfo] = useState({
+		savedPosts: [],
+		savedComments: [],
+		username: '',
+		email: '',
+		postCount: 0,
+		commentCount: 0,
+		// method to get user data after logging in
+		getUserData: () => {
+			// if user's logged in get the token or return null
+			const token = AuthService.loggedIn() ? AuthService.getToken() : null;
 
-      if (!token) {
-        return false;
-      }
-      API.getMe(token)
-        .then(({ data: { username, email, savedBooks, bookCount } }) =>
-          setUserInfo({ ...userInfo, username, email, savedBooks, bookCount })
-        )
-        .catch((err) => console.log(err));
-    },
-  });
+			if (!token) {
+				return false;
+			}
+			API.getMe(token)
+				.then(({ data: { username, email, savedPosts, savedComments, postCount, commentCount } }) =>
+					setUserInfo({ ...username, email, savedPosts, savedComments, postCount, commentCount })
+				)
+				.catch((err) => console.log(err));
+		},
+	});
 
-  // on load, get user data if a token exists
-  useEffect(() => {
-    userInfo.getUserData();
-  });
+	// on load, get user data if a token exists
+	useEffect(() => {
+		userInfo.getUserData();
+	});
 
-  return (
-    <Router>
-      <>
-        {/* wrap our entire app in context provider and provide userInfo state as value */}
-        <UserInfoContext.Provider value={userInfo}>
-          <Navbar />
-          <Switch>
-            <Route exact path='/' component={SearchBooks} />
-            <Route exact path='/saved' component={SavedBooks} />
-            <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-          </Switch>
-        </UserInfoContext.Provider>
-      </>
-    </Router>
-  );
+	return (
+		<Router>
+			<>
+				{/* wrap our entire app in context provider and provide userInfo state as value */}
+				<UserInfoContext.Provider value={userInfo}>
+					<Navbar />
+					<Switch>
+						<Route exact path="/" component={SearchBooks} />
+						<Route exact path="/saved" component={SavedBooks} />
+						<Route render={() => <h1 className="display-2">Wrong page!</h1>} />
+					</Switch>
+				</UserInfoContext.Provider>
+			</>
+		</Router>
+	);
 }
 
 export default App;
