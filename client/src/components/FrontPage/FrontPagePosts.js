77a2e48a6b7f0cf861b4b9f68fe59f7822fcebbd
Brandon A <jbrandonarnold@gmail.com>
2020-07-23 19:24:45 -0400

    added moment to new frontpage

--- client/src/components/FrontPage/FrontPagePosts.js
+++ client/src/components/FrontPage/FrontPagePosts.js
@@ -23,14 +23,8 @@ function FrontPagePosts(props) {
     }
     return (
         <CardGroup>
-<<<<<<< HEAD
-            <Card className="text-center">
-                <Card.Header style={styles.header}>{props.title}</Card.Header>
-                <Card.Img src={props.link} cap />
-=======
             <Card className="text-center card-hover">
                 <Card.Header style={styles.button}>{props.author}</Card.Header>
->>>>>>> 83619a13833acf35121cbb11a39be2874a2aa33d
                 <Card.Body>
                     <Card.Text>Poster: {props.author}</Card.Text>
                     <Card.Text>
