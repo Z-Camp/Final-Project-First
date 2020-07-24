74e9748a40b81ca00dcae195b21e8f46bfa6f4a8
Romo <60673021+Z-Camp@users.noreply.github.com>
2020-07-24 19:39:48 -0400

    github links open in new tab

--- client/src/components/AboutCard/AboutCard.js
+++ client/src/components/AboutCard/AboutCard.js
@@ -17,21 +17,15 @@ const styles = {
 function AboutCard() {
     return (
         <Card style={styles.card}>
-
             <Card.Body>
                 <Card.Title>Github Links</Card.Title>
                 <Card.Text>
-                    <Row className="links" ><a href="https://github.com/jdmmeli">Jason Meli</a></Row>
-                    <Row className="links"><a href="https://github.com/Z-Camp">Zach Campbell</a></Row>
-                    <Row className="links"><a href="https://github.com/jbrandona119" >Brandon Arnold</a></Row>
-                    <Row className="links"><a href="https://github.com/alecweems">Alec Weems</a></Row>
-
+                    <Row className="links" ><a href="https://github.com/jdmmeli" target="_blank">Jason Meli</a></Row>
+                    <Row className="links"><a href="https://github.com/Z-Camp" target="_blank">Zach Campbell</a></Row>
+                    <Row className="links"><a href="https://github.com/jbrandona119" target="_blank">Brandon Arnold</a></Row>
+                    <Row className="links"><a href="https://github.com/alecweems" target="_blank">Alec Weems</a></Row>
                 </Card.Text>
-
-
-
             </Card.Body>
-
         </Card>
     );
 
